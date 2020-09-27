cab3c0eb9f46e18fa54765d0df41d924a14fc13e
kamitom csw <csw@pm.me>
2020-09-28 00:46:26 +0800

    feat: importing packages morestrings

--- hello.go
+++ hello.go
@@ -1,10 +1,19 @@
 package main
 
-import f "fmt"
+import (
+	f "fmt"
+
+	morestr "github.com/kamitom/hello/morestrings"
+)
 
 func main() {
 	f.Println("hello, go modules")
 	f.Println("hello, 東京走著瞧!")
 	f.Println("hello, https://golang.org/doc/code.html !")
+	f.Println("hola, https://youtu.be/3ydlmgwZBbg !")
+
+	string_must_reverse := "!!123 !motimaK rehpog ,aloH"
+
+	f.Println(morestr.ReverseRunes(string_must_reverse))
 
 }
