9ab6484b701f51866235ce4ecb85415f5c8e9188
kamitom csw <csw@pm.me>
2020-09-28 20:43:47 +0800

    feat: use pkg stringrv

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/google/go-cmp v0.5.2
+	github.com/kamitom/stringrv v0.0.0-20200927044339-32381f069da1
 	rsc.io/quote v1.5.2
 	syreclabs.com/go/faker v1.2.2
+
 )
--- hello.go
+++ hello.go
@@ -9,17 +9,19 @@ import (
 
 	quote "rsc.io/quote"
 
+	stringrv "github.com/kamitom/stringrv"
 	fk "syreclabs.com/go/faker"
+	// gt "github.com/kamitom/greetingsgo"
 )
 
 func main() {
-	f.Println("hello, go modules2")
-	f.Println("hello, 東京走著瞧!")
+	f.Println("hello, go modules v2")
+	f.Println("hello, 東京走著瞧! v3")
 	f.Println("hello, https://golang.org/doc/code.html !")
 	f.Println("hola, https://youtu.be/3ydlmgwZBbg !")
 
 	string_must_reverse := "!!123 !motimaK rehpog ,aloH"
-	// string_must_reverse2 := "台灣"
+	string_must_reverse2 := "半沢 直樹"
 
 	f.Println(morestr.ReverseRunes(string_must_reverse))
 	f.Println(cmp.Diff("hello world", "hello go"))
@@ -29,5 +31,10 @@ func main() {
 	f.Println(quote.Go())
 
 	f.Println("Hola,", fk.Name().FirstName())
+
+	f.Println(stringrv.Reverse(string_must_reverse2))
+
+	// message := gt.Hello("tony stark")
+	// f.Println(message)
 
 }
